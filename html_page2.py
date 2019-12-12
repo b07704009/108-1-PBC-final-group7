32ec43eb7ec737fc8404fa72205b99559545103d
b07704009 <58811178+b07704009@users.noreply.github.com>
2019-12-12 19:23:45 +0800

    Create html_page2.py
    second page to website

---
+++ html_page2.py
@@ -0,0 +1,27 @@
+import sys
+import dominate
+from dominate import document
+from dominate import tags
+
+
+def main():
+    doc = dominate.document(title='record page')
+    
+    with doc.head:
+        tags.meta(name = 'charset', content = "utf-8")
+
+    with doc.body:
+        tags.h1('Register Page')
+        with tags.div():
+            tags.attr(cls='container')
+            
+            with tags.form(method='POST', action="/jump"):
+                with tags.legend('Please write down your name'):
+                    tags.input(type='text', name='name', size = 20)
+                with tags.legend('Please write down your number'):
+                    tags.input(type='text', name= 'number', size = 20)
+    
+    fn = 'templates/index2.html'
+    with open(file=fn, mode='w', encoding='utf-8') as f:
+        f.truncate
+        f = doc.render()
