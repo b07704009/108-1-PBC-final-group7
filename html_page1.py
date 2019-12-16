6911a1ede7a833bdd9c767c735290bce3e085612
b07704009 <58811178+b07704009@users.noreply.github.com>
2019-12-16 11:54:53 +0800

    Update html_page1.py

--- html_page1.py
+++ html_page1.py
@@ -1,13 +1,13 @@
 import sys
 from dominate import document
 from dominate import tags
+import dominate
 
 
 def main():
-    with document(title='Welcome Page') as doc:
+    doc = dominate.document(title='record page')
+    with doc.body:
         tags.h1('welcome to the page')
         tags.button('我是按鈕', onclick="location.href='http://127.0.0.1:5000/jump'")
-        print(doc.render())
-    fn = 'templates/index1.html'
-    with open(file=fn, mode='w', encoding='utf-8') as f:
-        f = doc.render()
+    print(doc.render())
+main()
